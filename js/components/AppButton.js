1a1d11146d003666e52d1a9a6787081d8c2ac2cb
Keiran West <127797224+GitHubWestie@users.noreply.github.com>
2025-07-10 07:55:43 +0000

    06. Component Props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,28 @@
 export default {
     template: `
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-2 py-1 disabled:cursor-not-allowed" :disabled="processing">
+        <button
+            :class="{
+                'border rounded px-2 py-1 disabled:cursor-not-allowed': true,
+                'bg-blue-500 hover:bg-blue-600 text-white': type === 'primary',
+                'bg-green-500 hover:bg-green-600 text-white': type === 'secondary',
+                'bg-gray-500 hover:bg-gray-600 text-white': type === 'muted',
+                'is-loading': processing,
+            }"
+            :disabled="processing"
+            >
             <slot />
         </button>
     `,
 
-    data() {
-        return {
-            processing: true
-        };
-    }
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+
+        processing: {
+            type: Boolean,
+            default: false,
+        }
+    },
 }
